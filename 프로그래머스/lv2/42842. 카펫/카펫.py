a9cb7a6a738a1c6847f96ed697c31d7100a2137c
Kim haewon <ehdvkf02@naver.com>
2022-08-04 00:42:02 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,10 +1,5 @@
 def solution(brown, yellow):
-    c = brown + yellow
-    result = []
-    for i in range(2, c+1):
-        if c % i == 0: result.append([c//i, i])
-    for x in result:
-        if 2 * (x[0] + x[1] - 2) == brown:
-            answer_x, answer_y = max(x[0], x[1]), min(x[0], x[1])
-            break
-    return [answer_x, answer_y ]
+    for x in range(2, int(brown/2)+1):
+        y = (brown+4)/2 - x
+        if (x*y == brown + yellow) and (x>=y):
+            return [x, y]
