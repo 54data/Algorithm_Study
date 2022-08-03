5313c33fffd6170c3399bc842535eb750213ca70
Kim haewon <ehdvkf02@naver.com>
2022-08-04 00:44:38 +0900

    [level 1] Title: 행렬의 덧셈, Time: 18.44 ms, Memory: 22.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,8 +1,8 @@
 def solution(arr1, arr2):
     answer = []
-    for i, j in zip(arr1, arr2):
-        result = []
-        for x, y in zip(i, j):
-            result.append(x+y)
-        answer.append(result)
+    for i, x in zip(arr1, arr2):
+        sum_result = []
+        for y, z in zip(i, x):
+            sum_result.append(y+z)
+        answer.append(sum_result)
     return answer
