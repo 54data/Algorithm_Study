7114c9e63f3bf04b0c6b73892211c6a8339350da
Kim haewon <ehdvkf02@naver.com>
2022-08-10 19:56:52 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,18 +1,40 @@
+
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
 def solution(progresses, speeds):
-    print(progresses)
-    print(speeds)
-    answer = []
-    time = 0
-    count = 0
-    while len(progresses)> 0:
-        if (progresses[0] + time*speeds[0]) >= 100:
-            progresses.pop(0)
-            speeds.pop(0)
-            count += 1
+    result = []
+    for i, x in zip(progresses, speeds):
+        if (100 - i) % x == 0:
+            result.append((100 - i) // x)
         else:
-            if count > 0:
-                answer.append(count)
-                count = 0
-            time += 1
-    answer.append(count)
+            result.append(((100 - i) // x) + 1)
+
+    a = result[0]
+    answer = []
+    cnt = 1
+    for i in range(1, len(result)):
+        if result[i] <= a:
+            cnt += 1
+        else:
+            a = result[i]
+            answer.append(cnt)
+            cnt = 1
+    answer.append(cnt)
     return answer
