866e20b85986067d75574d94564a1b41604ef56f
Kim haewon <ehdvkf02@naver.com>
2022-08-14 13:02:31 +0900

    [level 2] Title: 문자열 압축, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,20 +1,18 @@
 def solution(s):
     answer = len(s)
-    
-    for step in range(1, (len(s)//2)+1):
-        x = s[:step]
+
+    for i in range(1, (len(s)//2)+1):
         result = ''
+        start = s[:i]
         cnt = 1
-        
-        for j in range(step, len(s), step):
-            if x == s[j:j+step]:
+        for j in range(i, len(s), i):
+            if start == s[j:i+j]:
                 cnt += 1
             else:
-                result += str(cnt) + x if cnt >= 2 else x
-                x = s[j:j+step]
+                result += str(cnt) + start if cnt>1 else start
+                start = s[j:i+j]
                 cnt = 1
-                
-        result += str(cnt) + x if cnt >= 2 else x
-        
+        result += str(cnt) + start if cnt>1 else start
         answer = min(len(result), answer)
+
     return answer
