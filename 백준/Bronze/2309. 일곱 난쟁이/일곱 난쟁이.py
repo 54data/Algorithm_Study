be2f01a1398008a45b704d9712f8b324b8cc0b7f
Kim haewon <ehdvkf02@naver.com>
2022-08-15 16:42:54 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,16 +1,9 @@
-nlist = [int(input()) for i in range(9)]
+from itertools import combinations as cb
+nlist = [int(input()) for _ in range(9)]
+seven = list(cb(nlist, 7))
 
-X = sum(nlist) - 100
-
-for i in range(len(nlist)):
-    for x in range(i+1, len(nlist)):
-        if nlist[i] + nlist[x] == X:
-            d1, d2 = nlist[i], nlist[x]
-            break
-            
-nlist.remove(d1)
-nlist.remove(d2)
-nlist.sort()
-
-for i in nlist:
-    print(i)
+for i in seven:
+    if sum(i) == 100:
+        for j in sorted(list(i)):
+            print(j)
+        break
