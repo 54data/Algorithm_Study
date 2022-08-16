d77e801eda43dd7c16acb65ee863005ea204148a
Kim haewon <ehdvkf02@naver.com>
2022-08-16 15:04:57 +0900

    [level 2] Title: 타겟 넘버, Time: 2.44 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,17 @@
-answer = 0
-def dfs(max_len, cnt, result, numbers, target):
-    global answer
-    if cnt == max_len:
-        if result == target:
-            answer += 1
-        return
-    dfs(max_len, cnt+1, result+numbers[cnt], numbers, target)
-    dfs(max_len, cnt+1, result-numbers[cnt], numbers, target)
-    
 def solution(numbers, target):
     global answer
-    max_len = len(numbers)
-    cnt = 0
-    dfs(max_len, cnt,  0, numbers, target)
+    answer = 0
+    dfs(numbers, target, 0, 0)
     return answer
+
+def dfs(numbers, target, cnt, result):
+    global answer
+    if cnt == len(numbers):
+        if result == target:
+            answer += 1
+        else:
+            return
+    else:
+        dfs(numbers, target, cnt+1, result+numbers[cnt])
+        dfs(numbers, target, cnt+1, result-numbers[cnt])
+    
