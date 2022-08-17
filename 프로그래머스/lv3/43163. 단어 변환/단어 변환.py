b54e24f2fc766ec19bbbc44a4a701941b178fe31
Kim haewon <ehdvkf02@naver.com>
2022-08-18 01:21:00 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -3,21 +3,21 @@ from collections import deque
 def solution(begin, target, words):
     answer = 0
     queue = deque()
-    queue.append([begin, answer])
+    queue.append((begin, answer))
     result = False
-    
     while queue:
         x, answer = queue.popleft()
         if x == target:
             result = True
             break
-        for i in words:
-            cnt = 0
-            for a, b in zip(x, i):
-                if a == b:
-                    cnt += 1
-            if len(x) > cnt >= len(x)-1:
-                words.remove(i)
-                queue.append([i, answer+1])
-            
-    return answer if result != False else 0
+        else:
+            for i in words:
+                cnt = 0
+                for a, b in zip(i, x):
+                    if a == b:
+                        cnt += 1
+                if len(target) -1 <= cnt < len(target):
+                    words.remove(i)
+                    queue.append((i, answer+1))
+                    
+    return answer if result == True else 0
