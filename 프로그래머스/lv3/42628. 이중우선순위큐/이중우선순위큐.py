d524ae91e0b2666b76670d2aa2de6fbecf516baf
Kim haewon <ehdvkf02@naver.com>
2022-08-17 16:21:32 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,10 +1,16 @@
+import heapq
 def solution(operations):
     answer = []
+    heapq.heapify(answer)
     for i in operations:
-        if i[0] == 'I':
-            answer.append(int(i[i.find(' ')+1:]))
-            answer.sort()
-        elif answer != []:
-            if '-' in i: del answer[0]
-            else: del answer[-1]
+        a = i.split(' ')
+        if a[0] == 'I':
+            heapq.heappush(answer, int(a[1]))
+        if answer != []:
+            if a[0] == 'D':
+                if a[1] == '-1':
+                    heapq.heappop(answer)
+                else:
+                    answer = heapq.nlargest(len(answer), answer)[1:]
+                    heapq.heapify(answer)
     return [0, 0] if answer == [] else [max(answer), min(answer)]
