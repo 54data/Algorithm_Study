7719040f5c2c487a9f01e23e992058bbf019d15e
Kim haewon <ehdvkf02@naver.com>
2022-08-17 15:03:30 +0900

    [level 1] Title: 최소직사각형, Time: 3.09 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,7 +1,6 @@
 def solution(sizes):
-    for i in sizes:
-        if i[0] > i[1]:
-            i[0], i[1] = i[1], i[0]
-    max_a = sorted(sizes, key=lambda x:x[0], reverse=True)[0][0]
-    max_b = sorted(sizes, key=lambda x:x[1], reverse=True)[0][1]
-    return max_a * max_b
+    for i in range(len(sizes)):
+        if sizes[i][0] < sizes[i][1]:
+            sizes[i] = [sizes[i][1], sizes[i][0]]
+
+    return max(map(lambda x:x[0], sizes)) * max(map(lambda x:x[1], sizes))
