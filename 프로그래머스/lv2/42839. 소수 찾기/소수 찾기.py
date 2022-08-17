634fe6abe788613edcd7d428c0e2252bffddd70b
Kim haewon <ehdvkf02@naver.com>
2022-08-17 14:59:18 +0900

    [level 2] Title: 소수 찾기, Time: 0.48 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,22 +1,22 @@
-def sosu(x):
-    sosu_list = []
-    for i in x:
-        if i == 1 or i == 0:
-            pass
-        else:
-            for j in range(2, int(i**0.5)+1):
-                if i!=j and i%j==0:
-                    break
-            else:
-                sosu_list.append(i)
-    return len(sosu_list)
-
 from itertools import permutations as pm
+def sosu(x):
+    if x == 0 or x == 1:
+        return 0
+    for i in range(2, int(x*0.5)+1):
+        if x!=i and x%i==0:
+            return 0
+    else:
+        return 1
+        
 def solution(numbers):
     result = []
     for i in range(1, len(numbers)+1):
         result += list(pm(list(numbers), i))
+        
     result = list(map(lambda x:int(''.join(x)), result))
     result = list(set(result))
     result.sort()
-    return sosu(result)
+    cnt = 0
+    for i in result:
+        cnt += sosu(i)
+    return cnt
