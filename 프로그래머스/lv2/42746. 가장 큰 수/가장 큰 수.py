b184aab9c43fcd95aa4a4fbd1d3faa8276207b9f
Kim haewon <ehdvkf02@naver.com>
2022-08-17 16:36:36 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,16 +1,11 @@
-def check(n):
-    if len(n) == 1:
-        return n*6
+def leng(numbers):
+    if len(numbers) == 1:
+        return numbers * 6
     else:
-        return n*3
+        return numbers * 3
     
 def solution(numbers):
     numbers = list(map(lambda x:str(x), numbers))
-    numbers = sorted(numbers, key=lambda x:int(check(x)[:6]), reverse=True)
-    
-    answer = ''.join(numbers)
-    
-    if int(answer) != 0:
-        return answer
-    else:
-        return '0'
+    numbers = sorted(numbers, key=lambda x:int(leng(x)[:4]), reverse=True)
+    answer = ''.join(numbers) 
+    return answer if int(answer) != 0 else '0'
