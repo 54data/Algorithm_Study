b7cf228a1bdd28f7b9c580f96b93d1625f882756
Kim haewon <ehdvkf02@naver.com>
2023-02-28 12:00:45 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,18 @@
+def solution(s):
+    str_dict = {"(":")", "[":"]", "{":"}"}
+    answer = 0
+    
+    for i in range(len(s)):
+        stack = []
+        for j in s[i:]+s[:i]:
+            if stack and stack[-1] in str_dict.keys() and str_dict[stack[-1]] == j:
+                    stack.pop()
+            else:
+                stack.append(j)
+        if stack == []:
+            answer += 1
+    
+    return answer
+                    
+                    
+            
