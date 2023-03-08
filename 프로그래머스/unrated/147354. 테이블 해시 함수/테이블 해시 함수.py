31e08b269042f0e677f9fb55a298a79db8c0b8df
Kim haewon <ehdvkf02@naver.com>
2023-03-08 15:10:06 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,10 +1,13 @@
 def solution(data, col, row_begin, row_end):
-    answer = 0
+    answer = []
     data = sorted(data, key=lambda x:[x[col-1], -x[0]])
     
     for i in range(row_begin, row_end+1):
         result = 0
         for j in data[i-1]:
             result += j % i
-        answer ^= result
-    return answer
+        if not answer:
+            answer.append(result)
+        else:
+            answer[0] ^= result
+    return answer[0]
