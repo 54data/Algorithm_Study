96d4dc3b021eba14356ba49070fc2e1896e187de
Kim haewon <ehdvkf02@naver.com>
2023-03-16 19:36:43 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -0,0 +1,26 @@
+import math
+
+def get_gcd(arr):       
+    g = arr[0]
+    for i in range(1, len(arr)):
+        g = math.gcd(g, arr[i])     
+    return g
+
+def solution(arrayA, arrayB):
+    answer = 0
+    stat1, stat2 = True, True
+    gcd_A, gcd_B = get_gcd(arrayA), get_gcd(arrayB) 
+    
+    for i in range(len(arrayB)):     
+        if arrayB[i] % gcd_A == 0:
+            stat1 = False
+    if stat1:        
+        answer = max(answer, gcd_A)
+
+    for i in range(len(arrayA)):
+        if arrayA[i] % gcd_B == 0:
+            stat2 = False
+    if stat2:
+        answer = max(answer, gcd_B)
+
+    return answer
