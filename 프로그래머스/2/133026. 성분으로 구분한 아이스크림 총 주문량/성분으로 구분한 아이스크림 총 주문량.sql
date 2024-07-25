e69c140364d9ab9a316fc196c51eb621c874538e
Haewon Kim <ehdvkf02@naver.com>
2024-07-25 09:40:27 +0900

    [level 2] Title: 성분으로 구분한 아이스크림 총 주문량, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
@@ -0,0 +1,8 @@
+SELECT 
+    ii.ingredient_type,
+    SUM(total_order) AS total_order
+FROM first_half fh
+INNER JOIN icecream_info ii
+ON fh.flavor = ii.flavor
+GROUP BY ii.ingredient_type
+ORDER BY total_order;
